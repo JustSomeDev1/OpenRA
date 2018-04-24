a5566f293401b3c518befe2d0534096257916adc
Paul Chote <pchote@users.noreply.github.com>
2018-04-24 01:14:13 +0000

    more wip.

--- OpenRA.Mods.Common/Widgets/TouchScreenPaletteWidget.cs
+++ OpenRA.Mods.Common/Widgets/TouchScreenPaletteWidget.cs
@@ -18,14 +18,25 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Threading;
 using OpenRA.Graphics;
+using OpenRA.Mods.Common.Orders;
 using OpenRA.Mods.Common.Traits;
 using OpenRA.Mods.Common.Traits.Render;
 using OpenRA.Mods.Common.Widgets;
 using OpenRA.Network;
+using OpenRA.Primitives;
 using OpenRA.Widgets;
 
 namespace OpenRA.Mods.Common
 {
+	public class TouchIcon
+	{
+		public ActorInfo Actor;
+		public Pair<byte[], byte[]> Data;
+		public int DataSwitchRow;
+		public List<ProductionItem> Queued;
+		public ProductionQueue ProductionQueue;
+	}
+
 	public class TouchScreenPaletteWidget : Widget
 	{
 		readonly Queue<int2> clicks = new Queue<int2>();
@@ -38,13 +49,17 @@ namespace OpenRA.Mods.Common
 		readonly WorldRenderer worldRenderer;
 		readonly World World;
 
-		ProductionIcon[] icons = new ProductionIcon[12];
+		TouchIcon[] icons = new TouchIcon[10];
 		public int DisplayedIconCount { get; private set; }
 		public int IconRowOffset = 0;
 
-		public readonly int Columns = 4;
+		public readonly int Columns = 2;
+
+		public readonly string TabClick = null;
 
 		bool iconsDirty;
+
+		Dictionary<ActorInfo, Pair<byte[], byte[]>> iconCache = new Dictionary<ActorInfo, Pair<byte[], byte[]>>();
 
 		public ProductionQueue CurrentQueue
 		{
@@ -85,6 +100,51 @@ namespace OpenRA.Mods.Common
 			}
 		}
 
+		Pair<byte[], byte[]> LoadIcon(ActorInfo ai)
+		{
+			int[] ChannelMasks = { 2, 1, 0, 3 };
+
+			var rsi = ai.TraitInfo<RenderSpritesInfo>();
+			var icon = new Animation(World, rsi.GetImage(ai, World.Map.Rules.Sequences, null));
+
+			var bi = ai.TraitInfo<BuildableInfo>();
+			var palette = worldRenderer.Palette(bi.IconPalette);
+			icon.Play(bi.Icon);
+			var image = icon.Image;
+			var sheet = image.Sheet;
+			var imageData = sheet.GetData();
+
+			var normal = new byte[120 * 80 * 2];
+			var dark = new byte[120 * 80 * 2];
+
+			for (var j = 0; j < 40; j++)
+			{
+				for (var k = 0; k < 60; k++)
+				{
+					var sourceIndex = 4 * ((image.Bounds.Top + j + 2) * sheet.Size.Width + image.Bounds.Left + k + 2) + ChannelMasks[(int)image.Channel];
+					var c = palette.Palette.GetColor(imageData[sourceIndex]);
+					var normalHigh = (byte)((c.R & 0xF8) | (c.G >> 5));
+					var normalLow = (byte)(((c.G & 0xFC) << 3) | (c.B >> 3));
+					var darkHigh = (byte)((c.R & 0xE0) >> 2 | (c.G >> 7));
+					var darkLow = (byte)(((c.G & 0xF0) << 1) | (c.B >> 5));
+					//var normal565 = (ushort)(((c.R & 0xF8) << 8) | ((c.G & 0xFC) << 3) | (c.B >> 3));
+					//var dark565 = (ushort)(((c.R & 0xE0) << 6) | ((c.G & 0xF0) << 1) | (c.B >> 5));
+
+					normal[2 * (120 * j + 2 * k) + 0] = normalLow;
+					normal[2 * (120 * j + 2 * k) + 1] = normalHigh;
+					normal[2 * (120 * j + 2 * k) + 2] = normalLow;
+					normal[2 * (120 * j + 2 * k) + 3] = normalHigh;
+
+					dark[2 * (120 * j + 2 * k) + 0] = darkLow;
+					dark[2 * (120 * j + 2 * k) + 1] = darkHigh;
+					dark[2 * (120 * j + 2 * k) + 2] = darkLow;
+					dark[2 * (120 * j + 2 * k) + 3] = darkHigh;
+				}
+			}
+
+			return new Pair<byte[], byte[]>(normal, dark);
+		}
+
 		public void RefreshIcons()
 		{
 			var producer = CurrentQueue != null ? CurrentQueue.MostLikelyProducer() : default(TraitPair<Production>);
@@ -100,10 +160,13 @@ namespace OpenRA.Mods.Common
 			var rb = RenderBounds;
 			var faction = producer.Trait.Faction;
 
-			var buildables = AllBuildables.Skip(IconRowOffset * Columns).Take(12).ToList();
+			var buildables = AllBuildables.Skip(IconRowOffset * Columns).Take(10).ToList();
 			DisplayedIconCount = buildables.Count;
 
-			for (var i = 0; i < 12; i++)
+			var buildingSomething = currentQueue.AllQueued().Any();
+			var buildableItems = currentQueue.BuildableItems();
+
+			for (var i = 0; i < 10; i++)
 			{
 				if (i >= DisplayedIconCount)
 				{
@@ -112,21 +175,23 @@ namespace OpenRA.Mods.Common
 				}
 
 				var item = buildables[i];
-				var rsi = item.TraitInfo<RenderSpritesInfo>();
-				var icon = new Animation(World, rsi.GetImage(item, World.Map.Rules.Sequences, faction));
-				var bi = item.TraitInfo<BuildableInfo>();
-				icon.Play(bi.Icon);
+				var queued = currentQueue.AllQueued().Where(a => a.Item == item.Name).ToList();
+				if (!iconCache.ContainsKey(item))
+					iconCache[item] = LoadIcon(item);
 
-				icons[i] = new ProductionIcon()
+				var switchRow = buildingSomething || !buildableItems.Any(a => a.Name == item.Name) ? 0 : 80;
+				if (queued.Count > 0)
+				{
+					var first = queued[0];
+					switchRow = (first.TotalTime - first.RemainingTime) * 80 / first.TotalTime;
+				}
+
+				icons[i] = new TouchIcon()
 				{
 					Actor = item,
-					Name = item.Name,
-					Sprite = icon.Image,
-					Palette = worldRenderer.Palette(bi.IconPalette),
-					//IconClockPalette = worldRenderer.Palette(ClockPalette),
-					//IconDarkenPalette = worldRenderer.Palette(NotBuildablePalette),
-					//Pos = new int2(x, y).ToFloat2(),
-					Queued = currentQueue.AllQueued().Where(a => a.Item == item.Name).ToList(),
+					Data = iconCache[item],
+					DataSwitchRow = switchRow,
+					Queued = queued,
 					ProductionQueue = currentQueue
 				};
 			}
@@ -148,19 +213,76 @@ namespace OpenRA.Mods.Common
 			{
 				foreach (var c in clicks)
 				{
-					// TODO: Handle taps
+					// Check if it is an icon
+					var i = (c.X - 80) / 120;
+					var j = (c.Y - 30) / 80;
+
+					Console.WriteLine("{0} {1} {2}", c, i, j);
+
+					if (i >= 0 && i < 2 && j >= 0 && j < 5)
+					{
+						var icon = icons[j * 2 + i];
+						if (icon != null)
+						{
+							var item = icon.Queued.FirstOrDefault();
+							HandleLeftClick(item, icon, 1);
+						}
+					}
 				}
+
+				clicks.Clear();
 			}
+		}
+
+		protected bool PickUpCompletedBuildingIcon(TouchIcon icon, ProductionItem item)
+		{
+			var actor = World.Map.Rules.Actors[icon.Actor.Name];
+
+			if (item != null && item.Done && actor.HasTraitInfo<BuildingInfo>())
+			{
+				World.OrderGenerator = new PlaceBuildingOrderGenerator(CurrentQueue, icon.Actor.Name, worldRenderer);
+				return true;
+			}
+
+			return false;
+		}
+
+		bool HandleLeftClick(ProductionItem item, TouchIcon icon, int handleCount)
+		{
+			if (PickUpCompletedBuildingIcon(icon, item))
+			{
+				Game.Sound.Play(SoundType.UI, TabClick);
+				return true;
+			}
+
+			if (item != null && item.Paused)
+			{
+				// Resume a paused item
+				Game.Sound.Play(SoundType.UI, TabClick);
+				World.IssueOrder(Order.PauseProduction(CurrentQueue.Actor, icon.Actor.Name, false));
+				return true;
+			}
+
+			if (CurrentQueue.BuildableItems().Any(a => a.Name == icon.Actor.Name))
+			{
+				// Queue a new item
+				Game.Sound.Play(SoundType.UI, TabClick);
+				Game.Sound.PlayNotification(World.Map.Rules, World.LocalPlayer, "Speech", CurrentQueue.Info.QueuedAudio, World.LocalPlayer.Faction.InternalName);
+				World.IssueOrder(Order.StartProduction(CurrentQueue.Actor, icon.Actor.Name, handleCount));
+				return true;
+			}
+
+			return false;
 		}
 
 		unsafe void RenderLoop()
 		{
-			int[] ChannelMasks = { 2, 1, 0, 3 };
-
 			using (var mmf = MemoryMappedFile.CreateFromFile("/dev/fb1", FileMode.OpenOrCreate))
 			{
 				using (var accessor = mmf.CreateViewAccessor(0, 480 * 320 * 2, MemoryMappedFileAccess.ReadWrite))
 				{
+					var blankRow = new byte[240];
+
 					while (!stopRenderer)
 					{
 						// TODO: race conditions city
@@ -169,33 +291,24 @@ namespace OpenRA.Mods.Common
 							byte* ptr = (byte*)0;
 							accessor.SafeMemoryMappedViewHandle.AcquirePointer(ref ptr);
 
-							for (var i = 0; i < 12; i++)
+							for (var i = 0; i < 10; i++)
 							{
+								var x = (i % 2) * 120 + 80;
+								var y = (i / 2) * 80 + 35;
+
 								var icon = icons[i];
-								if (icon == null)
-									continue;
-
-								var x = (i % 4) * 120;
-								var y = (i / 4) * 80 + 74;
-
-								Console.WriteLine("{0} {1} {2}", i, x, y);
-
-								var iconData = icon.Sprite.Sheet.GetData();
-
-								// Blit line by line
-								for (var j = 0; j < 40; j++)
+								if (icon != null)
 								{
-									for (var k = 0; k < 60; k++)
+									// Blit line by line
+									for (var j = 0; j < 80; j++)
 									{
-										//var sourceIndex = (icon.Sprite.Bounds.Top * icon.Sprite.Sheet.Size.Width + icon.Sprite.Bounds.Left) * 4 + ChannelMasks[(int)icon.Sprite.Channel];
-										//var c = icon.Palette.Palette.GetColor(iconData[sourceIndex]);
-										var c = Color.RoyalBlue;
-										ptr[4 * (y * 240 + x)] = (byte)((c.R & 0xF8) | c.G >> 5);
-										ptr[4 * (y * 240 + x) + 1] = (byte)(((c.G & 0xFC) << 3) | (c.B >> 3));
-										ptr[4 * (y * 240 + x) + 2] = (byte)((c.R & 0xF8) | c.G >> 5);
-										ptr[4 * (y * 240 + x) + 3] = (byte)(((c.G & 0xFC) << 3) | (c.B >> 3));
+										var iconData = j < icon.DataSwitchRow ? icon.Data.First : icon.Data.Second;
+										Marshal.Copy(iconData, (j / 2) * 240, IntPtr.Add(new IntPtr(ptr), (j + y) * 320 * 2 + 2 * x), 240);
 									}
 								}
+								else
+									for (var j = 0; j < 80; j++)
+										Marshal.Copy(blankRow, 0, IntPtr.Add(new IntPtr(ptr), (j + y) * 320 * 2 + 2 * x), 240);
 							}
 
 							accessor.SafeMemoryMappedViewHandle.ReleasePointer();
@@ -226,7 +339,7 @@ namespace OpenRA.Mods.Common
 		void ProcessRawInput()
 		{
 			// TODO: This will change on reboot
-			using (var inputStream = new FileStream("/dev/input/event2", FileMode.Open, FileAccess.Read))
+			using (var inputStream = new FileStream("/dev/input/event3", FileMode.Open, FileAccess.Read))
 			{
 				var pos = 0;
 				var triggered = false;
@@ -246,8 +359,8 @@ namespace OpenRA.Mods.Common
 						pos = (int)data;
 					else if (triggered && type == 3 && code == 1)
 					{
-						var x = -((int)data - 3920) * 48 / 360;
-						var y = (pos - 270) * 32 / 360;
+						var x = (3900 - pos) * 32 / 360;
+						var y = (3900 - (int)data) * 48 / 360;
 						clicks.Enqueue(new int2(x, y));
 						triggered = false;
 					}
@@ -260,6 +373,5 @@ namespace OpenRA.Mods.Common
 			stopRenderer = true;
 			base.Removed();
 		}
-
 	}
 }
